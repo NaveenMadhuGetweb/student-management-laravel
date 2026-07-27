a5cfe442ccfe518eb3c6ddf389e9c42e0c365dac
Naveen Vision <naveenvisionmpvkn@gmail.com>
2026-07-27 11:24:25 -0700

    space removed changes

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,4 @@ Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name
 Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
 Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
 
-Route::resource('departments', DepartmentController::class); 
+Route::resource('departments', DepartmentController::class);
